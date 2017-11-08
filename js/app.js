f885f5d3ea9d33b432ca385577ebc7af33a04ee6
andreavr <anluu11@gmail.com>
2017-11-08 11:47:51 -0500

    añadiendo estructura base y efecto js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  var board = document.querySelector('.board-js');
+
+  board.addEventListener('click', addX);
+};
+
+var centinel = true;
+
+function addX(event) {
+  if (event.target.matches('td') && event.target.textContent === '') {
+    if (centinel)
+      event.target.textContent = 'x';
+    else
+      event.target.textContent = 'o';
+    centinel = !centinel;
+  }
+}
